b8c678c916b7e969b02b1d84e29be0bd999251b7
Travis Cline <travis.cline@gmail.com>
2019-08-31 14:01:02 -0700

    redact: Add identify lookup

--- cmd/redact/main.go
+++ cmd/redact/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 
 	dlp "cloud.google.com/go/dlp/apiv2"
@@ -12,22 +15,43 @@ import (
 )
 
 func main() {
+	flagInput := flag.String("f", "-", "input file to read")
+	flag.Parse()
+
+	in, err := fileToReader(*flagInput)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
 	ctx := context.Background()
-	if err := redact(ctx); err != nil {
+	if err := redact(ctx, in); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
-func redact(ctx context.Context) error {
+func redact(ctx context.Context, input io.Reader) error {
 	c, err := dlp.NewClient(ctx)
 	if err != nil {
 		return xerrors.Errorf("issue creating client: %w", err)
 	}
 
+	content, err := ioutil.ReadAll(input)
+	if err != nil {
+		return err
+	}
+
 	req := &dlppb.InspectContentRequest{
 		// TODO: Fill request struct fields.
 		Parent: fmt.Sprintf("projects/%v", os.Getenv("PROJECT_ID")),
+		Item: &dlppb.ContentItem{
+			DataItem: &dlppb.ContentItem_ByteItem{
+				ByteItem: &dlppb.ByteContentItem{
+					Data: content,
+				},
+			},
+		},
 	}
 	resp, err := c.InspectContent(ctx, req)
 	if err != nil {
@@ -35,4 +59,11 @@ func redact(ctx context.Context) error {
 	}
 	spew.Dump(resp)
 	return nil
+}
+
+func fileToReader(path string) (io.Reader, error) {
+	if path == "-" {
+		return os.Stdin, nil
+	}
+	return os.Open(path)
 }
