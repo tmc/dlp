e16741a8883b2df2a88350cac4933081624666ec
Travis Cline <travis.cline@gmail.com>
2020-04-28 18:04:26 -0700

    WIP

--- github-action/entrypoint.sh
+++ github-action/entrypoint.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-echo detect-pii $*
+#!/bin/bash
+set -x
+files=$(eval "${INPUT_DIFF}")
+(
+for f in $files; do
+    detect-pii -v 2 -f "${f}"
+done
+) | reviewdog -efm="%f:%l:%c: %m" -name="detect-pii" -reporter="${INPUT_REPORTER:-github-check}" -level="${INPUT_LEVEL}" -diff="${INPUT_DIFF}"
+
+
